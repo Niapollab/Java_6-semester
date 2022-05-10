235a217aa53ac00ca59525556d8ac1422676453f
Niapoll <kolya21082001@gmail.com>
2022-05-10 16:45:01 +0300

    Clean import in HashTable

--- Task 3/src/main/java/ru/vsu/collections/HashTable.java
+++ Task 3/src/main/java/ru/vsu/collections/HashTable.java
@@ -1,12 +1,6 @@
 package ru.vsu.collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Represents HashTable.

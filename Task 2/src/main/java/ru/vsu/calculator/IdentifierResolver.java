2ddb5f095d91b3a7df23c624d9b3bc2251242f1d
Niapoll <kolya21082001@gmail.com>
2022-05-12 13:40:46 +0300

    Add ConsolePolishNotationCalculatorIdentifierResolver

--- Task 2/src/main/java/ru/vsu/calculator/IdentifierResolver.java
+++ Task 2/src/main/java/ru/vsu/calculator/IdentifierResolver.java
@@ -2,9 +2,10 @@ package ru.vsu.calculator;
 
 import java.util.Stack;
 import ru.vsu.calculator.tokens.ValueToken;
+import ru.vsu.calculator.exceptions.*;
 
 public interface IdentifierResolver {
     double resolveVariable(ValueToken<String> token);
 
-    double resolveFunction(ValueToken<String> token, Stack<Double> allOperands);
+    double resolveFunction(ValueToken<String> token, Stack<Double> allOperands) throws PolishNotationCalculatingException;
 }

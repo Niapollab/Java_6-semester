a36576a5bbea18a063e3df3a9258c01acd7ff393
Niapoll <kolya21082001@gmail.com>
2022-05-12 14:25:41 +0300

    Add NumberTokenBuilderFactoryTests

--- Task 2/src/main/java/ru/vsu/Task.java
+++ Task 2/src/main/java/ru/vsu/Task.java
@@ -1,7 +1,35 @@
 package ru.vsu;
 
+import ru.vsu.calculator.exceptions.*;
+import ru.vsu.calculator.parsing.*;
+import ru.vsu.calculator.tokens.*;
+import ru.vsu.calculator.*;
+
+import java.util.*;
+
 public class Task {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws PolishNotationCalculatingException {
+        try {
+            List<TokenBuilderFactory> arr = new ArrayList<TokenBuilderFactory>();
+            arr.add(new NumberTokenBuilderFactory('.'));
+            arr.add(new OperatorTokenBuilderFactory());
+            arr.add(new IdentifierTokenBuilderFactory());
+            arr.add(new OpenBracketTokenBuilderFactory());
+            arr.add(new CloseBracketTokenBuilderFactory());
+            arr.add(new FunctionParamsSeparatorTokenBuilderFactory());
+
+            System.out.print("Read expression: ");
+            Scanner scanner = new Scanner(System.in);
+            TokenIterable it = new TokenIterable(scanner.nextLine(), arr);
+            Iterable<Token> tokens = ToPolishNotationConverter.convert(it);
+
+            PolishNotationCalculator calculator = new PolishNotationCalculator(new ConsolePolishNotationCalculatorIdentifierResolver(arr));
+
+            System.out.println(calculator.calculate(tokens));
+        } catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
     }
 }
